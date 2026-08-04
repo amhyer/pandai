2f14866c924ae1e63ad72b92280c456fbb2c1bc9
Z User <z@container>
2026-08-04 16:56:13 +0000

    37f2712b-da86-4892-b629-cc74dca7b490

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd /home/z/my-project
-export NODE_OPTIONS="--max-old-space-size=2048"
-exec npx next dev -p 3000
+export NODE_OPTIONS="--max-old-space-size=256"
+
+while true; do
+  echo "=== Starting Next.js dev server at $(date) ===" >> /home/z/my-project/dev.log
+  npx next dev -p 3000 >> /home/z/my-project/dev.log 2>&1
+  EXIT_CODE=$?
+  echo "=== Server exited with code $EXIT_CODE at $(date), restarting in 3s ===" >> /home/z/my-project/dev.log
+  sleep 3
+done
