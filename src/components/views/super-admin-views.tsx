e82f0601472c6946fab76643e11c6e46fa868232
amhyer <33901025+amhyer@users.noreply.github.com>
2026-08-29 09:43:27 +0800

    feat: add Tambah Pengguna to super admin users view

--- src/components/views/super-admin-views.tsx
+++ src/components/views/super-admin-views.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect, useCallback } from 'react';
 import {
   CardTitle,
   CardDescription,
